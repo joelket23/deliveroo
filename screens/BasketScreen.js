2189cb8253f3a7d4aa8c17d6c23eb357e858ba16
joelket23 <kushwahketan11@gmai.com>
2023-10-05 22:06:12 +0530

    basket top part ready

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,21 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useMemo } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectRestaurat } from '../features/restaurantSlice';
 import { selectBasketItems } from '../features/basketSlice';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { XCircleIcon } from 'react-native-heroicons/solid';
 
 const BasketScreen = () => {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurat);
   const items = useSelector(selectBasketItems);
+  const [groupItemsInBasket, setGroupItemsInBasket] = useState([]);
   const dispatch = useDispatch();
 
-  return (
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
-  )
-}
+  useMemo(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item.id] = results[item.id] || []).push(item);
+      return results;
+    }, {});
 
-export default BasketScreen
+    setGroupItemsInBasket(groupedItems);
+  }, [items]);
+  // console.log('setGroupItemsInBasket', groupItemsInBasket);
+
+  return (
+    // you can change here to simple view for better ui
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b bg-white shadow-xs border-[#00CCBB]">
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full text-gray-100 absolute top-3 right-5"
+          >
+            <XCircleIcon color={'#00CCBB'} height={50} width={50}></XCircleIcon>
+          </TouchableOpacity>
+        </View>
+        <View className='flex-row items-center space-x-4 px-4 py-3 bg-white my-5'>
+          <Image
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            source={{ uri: 'https://links.papareact.com/wru' }}
+          />
+          <Text className="">Deliver in 50-70 min</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00ccbb]">Change</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default BasketScreen;
